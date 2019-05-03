75f719fe96511927a2e3fffea10b3e5bfaa56e53
Brad Mortensen <b.mort86@gmail.com>
2019-05-03 10:13:00 -0600

    finishes ex1

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -8,15 +8,50 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
   HashTable *ht = create_hash_table(16);
 
   // YOUR CODE HERE
-
+  // Loop through array of weights
+  for (int i = 0; i < length; i++)
+  {
+    // Check if weight is less than the limit given
+    if (weights[i] <= limit)
+    {
+      // Check if limit minus the current weight exists in our hash table
+      int num1;
+      // Checks if function return is >= 0 to prevent -1 returns being saved
+      // assigns return to first variable
+      if((num1 = hash_table_retrieve(ht,limit - weights[i]))>=0)
+      {
+        // Conditional to make test pass by returning the answer in the right
+        if(i<num1)
+        {
+          // Mallocs space for answer struct and assigns idicies to it 
+          Answer *ans = malloc(sizeof(Answer));
+          ans->index_1 = num1;
+          ans->index_2 = i;
+          return ans;
+        }
+        else
+        {
+          Answer *a = malloc(sizeof(Answer));
+          a->index_1 = i;
+          a->index_2 = num1;
+          return a;
+        }        
+      }
+      // Adds the key values to the hash table key = weight, value = index
+      hash_table_insert(ht, weights[i], i);
+    }
+  }
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
@@ -28,22 +63,22 @@ int main(void)
   // TEST 1
   int weights_1 = {9};
   Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
-  print_answer(answer_1);  // NULL
+  print_answer(answer_1); // NULL
 
   // TEST 2
   int weights_2[] = {4, 4};
-  Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
-  print_answer(answer_2);  // {1, 0}
+  Answer *answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  print_answer(answer_2); // {1, 0}
 
   // TEST 3
   int weights_3[] = {4, 6, 10, 15, 16};
-  Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
-  print_answer(answer_3);  // {3, 1}
+  Answer *answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  print_answer(answer_3); // {3, 1}
 
   // TEST 4
   int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
-  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
-  print_answer(answer_4);  // {6, 2}
+  Answer *answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  print_answer(answer_4); // {6, 2}
 
   return 0;
 }
